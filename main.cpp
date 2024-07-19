48c7448b70a9d4c7c4cf99d830f7c5f7402f8c8a
Arham_Imran <marhamimran.mts42@gmail.com>
2024-07-19 10:40:50 +0500

    added checks in main.cpp for single linked list

--- main.cpp
+++ main.cpp
@@ -1,13 +1,26 @@
 #include <stdio.h>
 #include "Linked_List.hpp"
+using namespace std;
 
 int main()
 {
     ll::Linked_List<int> list;
+    list.print();
     list.insert_at_start(1);
+    list.print();
     list.insert_at_start(2);
+    list.print();
     list.insert_at_start(4);
+    list.print();
     list.insert_at_end(9);
+    list.print();
     list.insert_at_pos(5, 3);
     list.print();
+    cout << list[2].val << endl << list[2].next << endl;
+    list.delete_at_end();
+    list.print();
+    list.delete_at_start();
+    list.print();
+    list.delete_at_pos(1);
+    list.print(); 
 }
